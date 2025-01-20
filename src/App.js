d4f49cf2c11d316726c3ea489e927e67e83b563d
alexneumayr <alex.nmr@outlook.com>
2025-01-20 21:39:01 +0100

    Add comments

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { useState } from 'react';
 import ColorBlock from './ColorBlock';
 
 export default function App() {
-  const [color, setColor] = useState(randomColor());
-  const [hue, setHue] = useState('');
-  const [luminosity, setLuminosity] = useState('');
+  const [color, setColor] = useState(randomColor()); // state of the color which is being used for the box
+  const [hue, setHue] = useState(''); // contains the hue which the user types in the input field
+  const [luminosity, setLuminosity] = useState(''); // contains the luminosity which the user types in the input field
 
+  // onChange handler for the "hue" input field which sets the "hue" and "color" states accordingly
   function handleHueChange(event) {
     setHue(event.currentTarget.value);
     setColor(
@@ -15,16 +16,19 @@ export default function App() {
     );
   }
 
+  // onChange handler for the "luminosity" input field which sets the "hue" and "color" states accordingly
   function handleLuminosityChange(event) {
     setLuminosity(event.currentTarget.value);
     setColor(randomColor({ luminosity: event.currentTarget.value, hue: hue }));
   }
 
+  // onClick handler for the "Generate" button which sets the "color" state
   function handleGenerateButtonClick() {
     setColor(randomColor({ luminosity: luminosity, hue: hue }));
   }
 
   return (
+    // creates a div with the dimensions of the viewport and centers it's content
     <div
       style={{
         width: '100vw',
@@ -36,19 +40,23 @@ export default function App() {
       }}
     >
       <ColorBlock color={color} />
+      {/* calls the imported Component which displays the colored box */}
       <label>
         Hue:&nbsp;
         <input value={hue} onChange={handleHueChange} />
+        {/* input field for the hue */}
       </label>
       <label>
         Luminosity:&nbsp;
         <select onChange={handleLuminosityChange}>
+          {/* dropdown menu for the luminosity */}
           <option value="">random</option>
           <option value="dark">dark</option>
           <option value="light">light</option>
           <option value="bright">bright</option>
         </select>
       </label>
+      {/* "Generate" button */}
       <button
         style={{
           backgroundColor: '#000',
