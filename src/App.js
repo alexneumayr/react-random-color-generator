c4002e365a3d7f9ee9ef260df60eed326de18522
alexneumayr <alex.nmr@outlook.com>
2025-01-20 18:21:23 +0100

    Add styles

--- src/App.js
+++ src/App.js
@@ -4,20 +4,59 @@ import { useState } from 'react';
 
 export default function App() {
   const [color, setColor] = useState(randomColor());
-
   return (
-    <div>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
       <ColorBlock color={color} />
-      <button onClick={() => setColor(randomColor())}>Generate</button>
+      <button
+        style={{
+          backgroundColor: '#000',
+          color: 'white',
+          fontSize: '18px',
+          padding: '15px 20px',
+          marginTop: '10px',
+          width: '150px',
+        }}
+        onClick={() => setColor(randomColor())}
+      >
+        Generate
+      </button>
     </div>
   );
 }
 
 function ColorBlock(props) {
   return (
-    <div>
-      <h1>Generated color:</h1>
-      <p>{props.color}</p>
+    <div
+      style={{
+        border: 'solid 5px #000',
+        borderRadius: '15px',
+        width: '300px',
+        height: '300px',
+        backgroundColor: props.color,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '20px',
+          marginBottom: 0,
+        }}
+      >
+        Generated color:
+      </h1>
+      <p style={{ fontWeight: 'bold' }}>{props.color}</p>
     </div>
   );
 }
