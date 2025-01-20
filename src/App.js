658e19e73bf1661b0cfd1afd1afd010647980ef6
alexneumayr <alex.nmr@outlook.com>
2025-01-20 18:19:47 +0100

    Add ColorBlock component

--- src/App.js
+++ src/App.js
@@ -4,5 +4,19 @@ import { useState } from 'react';
 
 export default function App() {
   const [color, setColor] = useState(randomColor());
-  return <div></div>;
+
+  return (
+    <div>
+      <ColorBlock color={color} />
+    </div>
+  );
+}
+
+function ColorBlock(props) {
+  return (
+    <div>
+      <h1>Generated color:</h1>
+      <p>{props.color}</p>
+    </div>
+  );
 }
